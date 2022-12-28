c784aa9f689c2a8f53ca3c0ff0f882f7f4b86fbc
test <test@test.com>
2022-12-28 18:41:58 +0900

    4강 문제1 풀이 완료

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -2,12 +2,14 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
@@ -36,6 +38,27 @@ public class Lesson04Quiz01Controller {
 		// 응답값 jsp
 		return "lesson04/afterAddSeller";
 	}
+	
+	// http://localhost:8080/lesson04/quiz01/seller_info
+	// http://localhost:8080/lesson04/quiz01/seller_info?id=1
+	@GetMapping("/seller_info")
+	public String sellerInfo(
+			@RequestParam(value="id", required=false) Integer id,
+			Model model) {
+		
+		// db select
+		Seller seller = null;
+		if (id == null) {
+			seller = sellerBO.getLatestSeller();
+		} else {
+			seller = sellerBO.getSellerById(id);
+		}
+
+		model.addAttribute("seller", seller);
+		
+		return "lesson04/sellerInfo";
+	}
+	
 }
 
 
--- src/main/java/com/quiz/lesson04/bo/SellerBO.java
+++ src/main/java/com/quiz/lesson04/bo/SellerBO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quiz.lesson04.dao.SellerDAO;
+import com.quiz.lesson04.model.Seller;
 
 @Service
 public class SellerBO {
@@ -13,6 +14,14 @@ public class SellerBO {
 	
 	public void addSeller(String nickname, String profileImageUrl, Double temperature) {
 		sellerDAO.insertSeller(nickname, profileImageUrl, temperature);
+	}
+	
+	public Seller getLatestSeller() {
+		return sellerDAO.selectLatestSeller();
+	}
+	
+	public Seller getSellerById(int id) {
+		return sellerDAO.selectSellerById(id);
 	}
 }
 
