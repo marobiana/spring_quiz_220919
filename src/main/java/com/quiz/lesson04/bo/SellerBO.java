dbfc861505819eee2bc528c87e052dfa77a6b0f7
test <test@test.com>
2022-12-28 17:06:53 +0900

    4강 문제1-1 풀이

---
+++ src/main/java/com/quiz/lesson04/bo/SellerBO.java
@@ -0,0 +1,22 @@
+package com.quiz.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson04.dao.SellerDAO;
+
+@Service
+public class SellerBO {
+
+	@Autowired
+	private SellerDAO sellerDAO;
+	
+	public void addSeller(String nickname, String profileImageUrl, Double temperature) {
+		sellerDAO.insertSeller(nickname, profileImageUrl, temperature);
+	}
+}
+
+
+
+
+
